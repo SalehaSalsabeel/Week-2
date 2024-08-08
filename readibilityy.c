0519e128e504cd683a152e51b8a6856578299379
SalehaSalsabeel <salehasalsabeel@gmail.com>
2024-08-08 17:42:17 +0500

    Add files via upload

---
+++ readibilityy.c
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>URL</key>
+	<string>vscode-remote://codespaces%2Bliterate-spoon-5g4p659vr6953p7p5/workspaces/176919699/Readibility/readibility.c</string>
+</dict>
+</plist>
